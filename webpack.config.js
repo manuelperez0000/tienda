1759083d70797058218d6b35e780e10344f22db5
Manuel Perez <manuelperez.0000@gmail.com>
2020-07-31 16:38:43 -0400

    malo todavia

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,27 @@
+const { VueLoaderPlugin } = require('vue-loader');
+
 module.exports = {
-    entry: '../app/index.js',
-    output:{
-        path: __dirname+'public',
-        filename: 'bundle.js'
-    }
-}
+  entry: './src/app/index.js',
+  output: {
+    path: __dirname + './public/js/webpack',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+};
